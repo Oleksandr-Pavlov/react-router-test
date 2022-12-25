35a0bba275d467c45db8b5b6f8e4e5bdeabbc6ef
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-12-25 13:12:35 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-router-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
